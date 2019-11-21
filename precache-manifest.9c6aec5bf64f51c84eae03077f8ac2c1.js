490f04bbb39f0f923598308ac60753b97dfc9828
Debora Peralta <debora.peralta@furlongsolutions.com>
2019-11-21 13:52:54 +0000

    Updates

---
+++ precache-manifest.9c6aec5bf64f51c84eae03077f8ac2c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ea40212aaa34e121bc1cd1b19a009e",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "652c6e662dbf156edf79",
+    "url": "/monsters-react/static/css/main.00b9261e.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-react/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "652c6e662dbf156edf79",
+    "url": "/monsters-react/static/js/main.1ed1a33a.chunk.js"
+  },
+  {
+    "revision": "eac0f8dcfcc6931fb9ba",
+    "url": "/monsters-react/static/js/runtime-main.8a5d6a2e.js"
+  }
+]);
